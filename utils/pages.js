be95f861587ea0dbb39b3e0510d6956f33424b71
Sean C Davis <scdavis41@gmail.com>
2021-08-18 10:50:07 -0400

    Adds comments to utils and config file

--- utils/pages.js
+++ utils/pages.js
@@ -9,6 +9,15 @@ const pagesPattern = path.join(pagesDir, "**/*.md");
 
 const md = new MarkdownIt();
 
+/**
+ * Reads the pages directory and returns an object of key-value pairs where the
+ * key is the path of the page on the front end, and the value is the absolute
+ * path to the content file.
+ *
+ * Example:
+ *
+ *    { "/about/nested": "[CWD]/content/pages/about/nested.md" }
+ */
 async function getPageFilePathRefs() {
   const pagePaths = glob.sync(pagesPattern).map((pageFilePath) => {
     let pagePath = pageFilePath
@@ -21,16 +30,30 @@ async function getPageFilePathRefs() {
   return Object.fromEntries(pagePaths);
 }
 
+/**
+ * Extracts all the keys from getPageFilePathRefs() to present an array of all
+ * page paths for the site.
+ */
 export async function getPagePaths() {
   const pagePaths = await getPageFilePathRefs();
   return Object.keys(pagePaths);
 }
 
+/**
+ * Given a page path for the site, this returns an absolute path to its content
+ * file.
+ */
 async function getPageFileFromSlug(slug) {
   const pagePaths = await getPageFilePathRefs();
   return pagePaths[slug];
 }
 
+/**
+ * Given a path for a page on the site, this finds the content file, parses it,
+ * then adds `id` and `children` as additional props, where `id` is the path to
+ * the file from the root of the project, and `children` is the main content
+ * from the file, converted to an HTML string.
+ */
 export async function getPageProps(slug) {
   const pagePath = `/${(slug || []).join("/")}`;
   const pageFilePath = await getPageFileFromSlug(pagePath || "/");
@@ -43,6 +66,10 @@ export async function getPageProps(slug) {
   return props;
 }
 
+/**
+ * Reads all the pages in the content directory, and builds a list of links
+ * using the title of the page as the label.
+ */
 export async function getNavItems() {
   const pageFiles = await getPageFilePathRefs();
   const navItems = Object.entries(pageFiles).map(([slug, filePath]) => {
